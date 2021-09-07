9f593f75b4da5a097faac56c2c28c4b5ecfdbc8e
Ollie Payne <olliepayne@me.com>
2021-09-06 19:31:56 -0700

    Add: backup

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c9cfb970396bc8b3ba424312260e31df'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f136832b-1b83-4118-9170-bab0b0c4c48a'
+};
